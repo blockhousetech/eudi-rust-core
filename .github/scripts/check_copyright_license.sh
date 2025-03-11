6978ef6b9494d0915a17db595a67a1f1691812bf
mihaelmiklec <mihael@tbtl.com>
2025-03-11 12:39:01 +0100

    Set up initial CI (#1)
    The pipeline performs various code quality and consistency checks such
    as:
     - linting with `clippy`
     - checking that the code is formatted according to TBTL's settings
     - checking for unused dependencies, vulnerabilities in used dependencies,
       disallowed licenses, banned crates, etc.
     - building docs
     - building and running tests across various OSs and CPU architectures
    
    Additionally, a script was added to check if all the Rust files have a
    header with the proper copyright & license notice.
    
    3rd-party actions were anchored to specific commits so they are used
    as immutable releases - mitigating the risk of a bad actor subsequently
    adding a backdoor through a branch or tag update.

---
+++ .github/scripts/check_copyright_license.sh
@@ -0,0 +1,50 @@
+#!/bin/bash
+#
+# Checks if all the Rust files in the project have the correct copyright
+# & license header.
+
+set -euo pipefail
+
+readonly NOTICE=$(cat << EOF
+// Copyright (C) 2020-2025  The Blockhouse Technology Limited (TBTL).
+//
+// This program is free software: you can redistribute it and/or modify it
+// under the terms of the GNU Affero General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or (at your
+// option) any later version.
+//
+// This program is distributed in the hope that it will be useful, but
+// WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY
+// or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU Affero General Public
+// License for more details.
+//
+// You should have received a copy of the GNU Affero General Public License
+// along with this program.  If not, see <https://www.gnu.org/licenses/>.
+EOF
+)
+
+# Exclude `*.rs` files in any `target/` folder
+RS_FILES=$(
+  find . \
+    -type d -name target -prune -o \
+    -type f -name '*.rs' -print
+)
+
+files_missing_notice=()
+
+for file in ${RS_FILES[@]}; do
+  header=$(head -n "$(wc -l <<< "$NOTICE")" "${file}")
+  if [[ "${header}" != "${NOTICE}" ]]; then
+    files_missing_notice+=("${file}")
+  fi
+done
+
+if [[ ${#files_missing_notice[@]} -gt 0 ]]; then
+  echo "ERROR: The following files are missing or have an incorrect copyright notice:"
+  for file in "${files_missing_notice[@]}"; do
+    echo "  - ${file}"
+  done
+  exit 1
+fi
+
+echo "All *.rs files have the correct notice!"
